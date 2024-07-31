731a5652818bd07b3e3ce8203d2c7261a70bd46f
Eason_chiang <95297596+class83108@users.noreply.github.com>
2024-07-31 10:14:31 +0800

    [fix]修改js位置

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
 
-console.log("Hello world!");
+const chooseTypeBtns = document.querySelectorAll(".choose_type_btn");
+
+if (chooseTypeBtns) {
+  chooseTypeBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const value = e.target.getAttribute("data-target");
+      localStorage.setItem("seriesType", value);
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const seriesType = localStorage.getItem("seriesType");
+    if (seriesType) {
+      const targetBtn = document.getElementById(seriesType);
+      targetBtn.classList.add("series_active");
+    }
+  });
+}
